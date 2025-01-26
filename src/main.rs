b513bf4b404ff706701c86479d9cfeec069887e4
thatmagicalcat <thatmagicalcat@duck.com>
2025-01-26 22:58:40 +0530

    added new windows

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,26 @@ use dwm_statusbar::*;
 fn main() {
     StatusBar::new(
         " | ",
+        1,
         vec![
             blocks! {
-            //  { function         , update interval (ms) }
-                { |_| String::new(), u64::MAX  }, // to add an extra `|` at the start
-                { |_| cpu()        , 5_000     },
-                { |_| battery()    , 20_000    },
-                { |_| ram()        , 10_000    },
-                { |_| storage()    , 60_000    },
-                { |_| date()       , 30_000    },
+                { |_| "windows: 1 -> status, 2 -> media, 3 -> misc".to_string(), 100 }
             },
             blocks! {
-                { |_| volume()     , 100       },
-                { |_| brtns()      , 100       },
+            //  { function         , update interval (ms) }
+                { |_| String::new(), 1_000     }, // to add an extra `|` at the start
+                { |_| cpu(), 5_000 },
+                { |_| battery(), 20_000 },
+                { |_| ram(), 10_000 },
+                { |_| storage(), 60_000 },
+                { |_| date(), 30_000 },
+            },
+            blocks! {
+                { |_| media().unwrap_or_else(|| String::from("loading...")), 1000 },
+            },
+            blocks! {
+                { |_| volume() , 100 },
+                { |_| brtns() , 100 },
             },
         ],
     )
